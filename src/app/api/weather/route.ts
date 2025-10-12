5444d9ce11ab1a9f08fb0ff852c38197efe196af
AlexWallot <alexwallot@outlook.com>
2025-10-12 17:01:26 -0400

    retrait du force-dynamic

--- src/app/api/weather/route.ts
+++ src/app/api/weather/route.ts
@@ -1,9 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import axios from 'axios';
 
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
-
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const lat = searchParams.get('lat');
